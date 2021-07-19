66657d5160186df258a26137be16592571767e51
Guilherme Girotto <guilherme.girotto@raccoon.ag>
2021-07-19 08:40:12 -0300

    componente transferencia criado

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,16 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovaTransferenciaComponent {
 
-
+  @Output() aoTransferir = new EventEmitter<any>();
 
    valor!: number;
    destino!: number;
 
 
    transferir(){
-     console.log('alou', this.valor);
-     console.log('destino', this.destino); 
+     const valorEmitir ={valor: this.valor, destino: this.destino};
+      this.aoTransferir.emit(valorEmitir);
+      this.limparCampos();
     }
 
+    limparCampos(){
+      this.valor = 0;
+      this.destino = 0;
+    }
 
 }
