b9e8856639b81503570db93a5b188ed87e5c4ce0
Guilherme Girotto <guilherme.girotto@raccoon.ag>
2021-07-19 16:36:18 -0300

    finalizado

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { Transferencia } from '../services/models/transferencia';
 import { TransferenciaService } from '../services/transferencia.service';
 
@@ -14,7 +15,7 @@ export class NovaTransferenciaComponent {
   valor!: number;
   destino!: number;
 
-  constructor(private service: TransferenciaService) { }
+  constructor(private service: TransferenciaService, private router: Router) { }
 
 
   transferir() {
@@ -22,6 +23,7 @@ export class NovaTransferenciaComponent {
     this.service.adicionar(valorEmitir).subscribe(resultado => {
       console.log(resultado);
       this.limparCampos();
+      this.router.navigateByUrl('extrato');
     },
       error => console.error(error));
 
